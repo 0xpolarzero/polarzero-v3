579ed8b0693e1074f9bcb2a8ae835a649405efc5
polarzero <0xpolarzero@gmail.com>
2023-02-17 19:47:19 +0100

    Update with real data

--- src/data/work/work-en.js
+++ src/data/work/work-en.js
@@ -6,16 +6,36 @@ import imageMetaverse from '@/assets/images/screenshot_metaverse.png';
 const work = [
   {
     type: types[0],
-    title: 'Name of project',
-    description: 'Description of project, some details, etc.',
+    title: 'Promise',
+    description: (
+      <div>
+        A blockchain-based platform that provides transparency and
+        accountability in digital relationships, allowing users to keep track of
+        the reliability of a person or a team. Essentially, it's a way to ensure
+        that founders can be held accountable for their promises, by allowing
+        them to make a genuine commitment that cannot be altered.
+      </div>
+    ),
+    context: (
+      <div>
+        Built during the Chainlink Fall 2022 Hackathon. QuickNode 1st Prize &
+        Chainlink Top 40 Quality Projects.
+      </div>
+    ),
     link: 'https://usepromise.xyz',
     image: imagePromise,
     date: '2020-01-01',
   },
   {
     type: types[1],
-    title: 'Name of project',
-    description: 'Description of project, some details, etc.',
+    title: 'Immersive Audio',
+    description: (
+      <div>
+        An example of 3D audio integration in a virtual world on the Web, to
+        showcase the major benefits of immersive audio in such environments
+      </div>
+    ),
+    context: <div>Built using the Atmoky Web audio engine.</div>,
     link: 'https://immersiveaudio.polarzero.xyz',
     image: imageMetaverse,
     date: '2020-01-01',

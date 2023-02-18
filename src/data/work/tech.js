4a408fddc5c1008ebd266e4167eb5e9aeaef9e26
polarzero <0xpolarzero@gmail.com>
2023-02-18 16:44:51 +0100

    Add tech stack icons

---
+++ src/data/work/tech.js
@@ -0,0 +1,123 @@
+const tech = {
+  alchemy: {
+    name: 'Alchemy',
+    id: 'alchemy',
+    link: 'https://alchemy.com/',
+  },
+
+  antd: {
+    name: 'Ant Design',
+    id: 'antd',
+    link: 'https://ant.design/',
+  },
+
+  apollographql: {
+    name: 'Apollo GraphQL',
+    id: 'apollographql',
+    link: 'https://www.apollographql.com/',
+  },
+
+  arweave: {
+    name: 'Arweave',
+    id: 'arweave',
+    link: 'https://www.arweave.org/',
+  },
+
+  atmoky: {
+    name: 'Atmoky',
+    id: 'atmoky',
+    link: 'https://atmoky.com/',
+  },
+
+  bundlr: {
+    name: 'Bundlr',
+    id: 'bundlr',
+    link: 'https://bundlr.network/',
+  },
+
+  chainlink: {
+    name: 'Chainlink',
+    id: 'chainlink',
+    link: 'https://chain.link/',
+  },
+
+  ethers: {
+    name: 'Ethers',
+    id: 'ethers',
+    link: 'https://docs.ethers.io/v5/',
+  },
+
+  hardhat: {
+    name: 'Hardhat',
+    id: 'hardhat',
+    link: 'https://hardhat.org/',
+  },
+
+  ipfs: {
+    name: 'IPFS',
+    id: 'ipfs',
+    link: 'https://ipfs.io/',
+  },
+
+  javascript: {
+    name: 'JavaScript',
+    id: 'javascript',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+
+  nextjs: {
+    name: 'Next.js',
+    id: 'nextjs',
+    link: 'https://nextjs.org/',
+  },
+
+  nodejs: {
+    name: 'Node.js',
+    id: 'nodejs',
+    link: 'https://nodejs.org/',
+  },
+
+  react: {
+    name: 'React',
+    id: 'react',
+    link: 'https://reactjs.org/',
+  },
+
+  reactThreeFiber: {
+    name: 'React Three Fiber',
+    id: 'pmndrs',
+    link: 'https://docs.pmnd.rs/react-three-fiber/',
+  },
+
+  solidity: {
+    name: 'Solidity',
+    id: 'solidity',
+    link: 'https://soliditylang.org/',
+  },
+
+  theGraph: {
+    name: 'The Graph',
+    id: 'thegraph',
+    link: 'https://thegraph.com/',
+  },
+
+  threejs: {
+    name: 'Three.js',
+    id: 'threejs',
+    link: 'https://threejs.org/',
+  },
+
+  wagmi: {
+    name: 'Wagmi',
+    id: 'wagmi',
+    link: 'https://wagmi.sh/',
+  },
+
+  zustand: {
+    name: 'Zustand',
+    id: 'pmndrs',
+    link: 'https://github.com/pmndrs/zustand',
+  },
+};
+
+export default tech;

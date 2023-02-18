c2c80660d207d999bc4ae16b8834ed304f9e2a99
polarzero <0xpolarzero@gmail.com>
2023-02-18 16:44:54 +0100

    Update types.js

--- src/data/work/types.js
+++ src/data/work/types.js
@@ -1,7 +1,3 @@
-const types = [
-  'Smart Contracts',
-  'React Three Fiber',
-  'Smart Contracts & React Three Fiber',
-];
+const types = [['smart_contracts'], ['web_3d'], ['smart_contracts', 'web_3d']];
 
 export default types;

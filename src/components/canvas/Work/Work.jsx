5b23e69d5d82b84cccd2be3de9df525f68d152b6
polarzero <0xpolarzero@gmail.com>
2023-02-18 16:44:45 +0100

    Update card display & add Image

--- src/components/canvas/Work/Work.jsx
+++ src/components/canvas/Work/Work.jsx
@@ -1,9 +1,10 @@
-import * as THREE from 'three';
 import { useEffect, useRef, useState } from 'react';
+import { Image } from 'next/image';
+import * as THREE from 'three';
 import { useFrame, useThree } from '@react-three/fiber';
 import {
+  Image as ImageDrei,
   useIntersect,
-  Image,
   ScrollControls,
   Scroll,
   useScroll,
@@ -12,6 +13,8 @@ import {
 import config from '@/data';
 import Entity from './Entity';
 import stores from '@/stores';
+import { Tooltip } from 'antd';
+import CounterHelper from './CounterHelper';
 
 const Work = () => {
   const projects = config.work['en'];
@@ -24,6 +27,31 @@ const Work = () => {
       {/* <Items /> */}
       <Entity />
       <CounterHelper projects={projects} />
+
+      <Scroll>
+        {width < 9.6
+          ? null
+          : projects.map((project, i) => {
+              const scaleBase = [1 * (project.image.x / project.image.y), 1, 1];
+              const scale = scaleBase.map((s) => s * (width / height) * 1.5);
+              const offset = width > 20 ? (width - 20) / 2 : 0;
+              const position = [
+                (i % 2 === 0 ? -1 : 1) * (scale[0] * 1.5) +
+                  (i % 2 === 0 ? offset : -offset),
+                (i + 1) * -height,
+                0,
+              ];
+
+              return (
+                <ItemImage
+                  key={i}
+                  url={project.image.url}
+                  position={position}
+                  scale={scale}
+                />
+              );
+            })}
+      </Scroll>
       <Scroll html style={{ width: '100%' }}>
         <h1
           style={{
@@ -35,40 +63,27 @@ const Work = () => {
           }}>
           polarzero
         </h1>
-        {projects.map((project, i) => (
-          <Item
-            key={i}
-            data={project}
-            positionY={(i + 2) * 100}
-            positionX={i % 2 === 0 ? 'right' : 'left'}
-          />
-        ))}
+        <div className='work'>
+          {projects.map((project, i) => (
+            <Item
+              key={i}
+              data={project}
+              positionY={(i + 2) * 100}
+              positionX={i % 2 === 0 ? 'right' : 'left'}
+            />
+          ))}
+        </div>
       </Scroll>
     </ScrollControls>
   );
 };
 
-const CounterHelper = ({ projects }) => {
-  // Detect the visible project based on scroll position
-  const setVisibleProject = stores.useCounter(
-    (state) => state.setVisibleProject,
-  );
-  const data = useScroll();
-
-  useFrame(() => {
-    const index = Math.round(data.offset * projects.length);
-    const project = index === 0 ? null : projects[index - 1];
-    setVisibleProject(project);
-  });
-
-  return null;
-};
-
 function Item({ data, positionY, positionX }) {
+  const theme = stores.useConfig((state) => state.theme);
+  const { width, height } = useThree((state) => state.viewport);
   const visible = useRef(false);
   const [hovered, hover] = useState(false);
   const ref = useIntersect((isVisible) => (visible.current = isVisible));
-  const { width, height } = useThree((state) => state.viewport);
 
   useFrame((state, delta) => {
     ref.current.style.transform = `translate3d(0,${THREE.MathUtils.damp(
@@ -82,9 +97,7 @@ function Item({ data, positionY, positionX }) {
   return (
     <div
       ref={ref}
-      className={`work-item ${positionX}`}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
+      className={`work-item ${positionX} ${width < 9.6 ? 'mobile' : 'desktop'}`}
       style={{
         position: 'absolute',
         top: `${positionY}vh`,
@@ -92,15 +105,77 @@ function Item({ data, positionY, positionX }) {
         width: '100%',
         height: '100vh',
       }}>
-      <div className='title'>{data.title}</div>
-      <div className='type'>{data.type}</div>
-      <div className='description'>{data.description}</div>
-      <div className='context'>{data.context}</div>
-      <div className='link'>{data.link}</div>
-      <div className='image'>{/* <Image url={data.image} /> */}</div>
-      <div className='date'>{data.date}</div>
+      <div
+        className={`card ${hovered ? 'hovered' : ''}`}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}>
+        <div className='header'>
+          <span className='title'>{data.title}</span>
+          <span className='type'>{data.type.join(' - ')}</span>
+        </div>
+        <div className='description'>{data.description}</div>
+        <div className='context'>{data.context}</div>
+        <div className='links'>{data.links}</div>
+        <div className='tech'>
+          {data.tech.map((tech, i) => (
+            <Tooltip title={tech.name} key={i} className='tech-item'>
+              <a href={tech.link} target='_blank' rel='noreferrer'>
+                <img
+                  src={`/images/tech/${theme}/${tech.id}.svg`}
+                  alt={tech.name}
+                  height={30}
+                />
+              </a>
+            </Tooltip>
+          ))}
+        </div>
+        <div className='image'>{/* <Image url={data.image} /> */}</div>
+        <div className='date'>{data.date}</div>
+      </div>
     </div>
   );
 }
+
+const ItemImage = ({ url, position, scale }) => {
+  const { width, height } = useThree((state) => state.viewport);
+  const [hovered, hover] = useState(false);
+
+  const visible = useRef(false);
+  const ref = useIntersect((isVisible) => (visible.current = isVisible));
+
+  useFrame((state, delta) => {
+    ref.current.position.y = THREE.MathUtils.damp(
+      ref.current.position.y,
+      visible.current ? 0 : -height / 2 + 1,
+      4,
+      delta,
+    );
+    ref.current.material.zoom = THREE.MathUtils.damp(
+      ref.current.material.zoom,
+      visible.current ? 1 : 1.5,
+      4,
+      delta,
+    );
+    ref.current.material.grayscale = THREE.MathUtils.damp(
+      ref.current.material.grayscale,
+      hovered ? 0 : 1,
+      4,
+      delta,
+    );
+  });
+
+  return (
+    <group position={position}>
+      <ImageDrei
+        ref={ref}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        scale={scale}
+        url={url}
+        style={{ zIndex: hovered ? 100000 : 0 }}
+      />
+    </group>
+  );
+};
 
 export default Work;

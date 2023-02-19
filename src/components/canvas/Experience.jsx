8bf6c281d73c63b4706c00f1726879f911087765
polarzero <0xpolarzero@gmail.com>
2023-02-19 19:41:03 +0100

    Add About

--- src/components/canvas/Experience.jsx
+++ src/components/canvas/Experience.jsx
@@ -1,6 +1,8 @@
 import Work from './Work';
+import About from './About';
 
 const Experience = ({ ...props }) => {
+  if (props.type === 'about') return <About {...props} />;
   return <Work {...props} />;
 };
 

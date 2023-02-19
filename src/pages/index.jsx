6c50906437a72920c70b3037047b33cb07193e8d
polarzero <0xpolarzero@gmail.com>
2023-02-19 11:24:49 +0100

    Add type

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,6 @@
 import dynamic from 'next/dynamic';
 import Home from '@/components/dom/Home';
 
-// Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
-// WARNING ! errors might get obfuscated by using dynamic import.
-// If something goes wrong go back to a static import to show the error.
-// https://github.com/pmndrs/react-three-next/issues/49
 const Experience = dynamic(() => import('@/components/canvas/Experience'), {
   ssr: false,
 });
@@ -14,9 +10,7 @@ export default function Page(props) {
   return <Home />;
 }
 
-// Canvas components go here
-// It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Experience />;
+Page.canvas = (props) => <Experience type='work' />;
 
 export async function getStaticProps() {
   return { props: { title: 'polarzero' } };

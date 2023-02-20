fe8a07dbc7f5177cfbb34943b42973f2931bd626
polarzero <0xpolarzero@gmail.com>
2023-02-20 12:44:27 +0100

    Move CounterHelper

--- src/components/canvas/CounterHelper.jsx
+++ src/components/canvas/CounterHelper.jsx
@@ -4,12 +4,15 @@ import stores from '@/stores';
 
 const CounterHelper = ({ content }) => {
   // Detect the visible project based on scroll position
-  const setVisibleProject = stores.useCounter(
-    (state) => state.setVisibleProject,
-  );
+  const { setVisibleProject, setScrollPos } = stores.useCounter((state) => ({
+    setVisibleProject: state.setVisibleProject,
+    setScrollPos: state.setScrollPos,
+  }));
   const data = useScroll();
 
   useFrame(() => {
+    setScrollPos(data.offset);
+
     const index = Math.round(data.offset * content.length);
     const project = index === 0 ? null : content[index - 1];
     setVisibleProject(project);

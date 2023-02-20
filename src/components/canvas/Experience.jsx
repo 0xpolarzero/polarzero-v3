5e5ac300503aad0c6951e0b39f54b7cd1a98c6dc
polarzero <0xpolarzero@gmail.com>
2023-02-20 12:44:55 +0100

    Add Immersive conditional rendering

--- src/components/canvas/Experience.jsx
+++ src/components/canvas/Experience.jsx
@@ -1,9 +1,19 @@
-import Work from './Work';
+import Entity from './Entity';
+import Immersive from './Immersive';
 import About from './About';
+import stores from '@/stores';
 
 const Experience = ({ ...props }) => {
+  const readingMode = stores.useCounter((state) => state.readingMode);
+
   if (props.type === 'about') return <About {...props} />;
-  return <Work {...props} />;
+
+  return (
+    <>
+      <Entity {...props} />
+      {readingMode ? null : <Immersive {...props} />}
+    </>
+  );
 };
 
 export default Experience;

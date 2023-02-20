fec8639f030bfe3c848ac001fa277af571faa75e
polarzero <0xpolarzero@gmail.com>
2023-02-20 12:49:14 +0100

    Add scrollPos, readingMode & activePage

--- src/stores/useCounter.js
+++ src/stores/useCounter.js
@@ -1,6 +1,27 @@
 import { create } from 'zustand';
+import useConfig from './useConfig';
+import config from '@/data';
 
-export default create((set) => ({
+export default create((set, get) => ({
+  // Scroll
+  scrollPos: 0,
+  setScrollPos: (scrollPos) => set({ scrollPos }),
+
+  // Visible project
   visibleProject: null,
   setVisibleProject: (project) => set({ visibleProject: project }),
+
+  // Active page
+  activePage: config.nav[useConfig.getState().language][0],
+  setActivePage: (page) => set({ activePage: page }),
+
+  // Reading mode
+  readingMode: false,
+  setReadingMode: (readingMode) => set({ readingMode }),
+  toggleReadingMode: () => {
+    const { readingMode, setScrollPos } = get();
+
+    if (readingMode) setScrollPos(0);
+    set({ readingMode: !readingMode });
+  },
 }));
